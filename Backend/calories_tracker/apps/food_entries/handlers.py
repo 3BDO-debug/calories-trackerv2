1db6c0b045c78ae8a3d1aea0854779d0764790d6
Sourcery AI <>
2023-04-07 19:54:56 +0000

    'Refactored by Sourcery'

--- Backend/calories_tracker/apps/food_entries/handlers.py
+++ Backend/calories_tracker/apps/food_entries/handlers.py
@@ -44,79 +44,78 @@ def food_entries_handler(request, user_id):
 @api_view(["GET", "POST", "PUT", "DELETE"])
 def admin_food_entries_handler(request):
 
-    if request.user.is_superuser:
-        if request.method == "GET":
-            food_entries = models.FoodEntry.objects.all().order_by("-timestamp")
-            food_entries_serializer = serializers.FoodEntrySerializer(
-                food_entries, many=True
-            )
-
-            return Response(
-                data=food_entries_serializer.data, status=status.HTTP_200_OK
-            )
-
-        elif request.method == "DELETE":
-
-            models.FoodEntry.objects.get(
-                id=int(request.data.get("foodEntryId"))
-            ).delete()
-
-            food_entries = models.FoodEntry.objects.all()
-            food_entries_serializer = serializers.FoodEntrySerializer(
-                food_entries, many=True
-            )
-
-            return Response(
-                data=food_entries_serializer.data, status=status.HTTP_200_OK
-            )
-
-        elif request.method == "PUT":
-            food_entry = models.FoodEntry.objects.get(
-                id=int(request.data.get("foodEntryId"))
-            )
-
-            if request.data.get("foodName"):
-                food_entry.food_name = request.data.get("foodName")
-
-            if request.data.get("calories"):
-                food_entry.calories = int(request.data.get("calories"))
-
-            if request.data.get("timestamp"):
-                parsed_datetime = datetime.datetime.strptime(
-                    request.data.get("timestamp"), "%Y-%m-%d %H:%M"
-                )
-                food_entry.timestamp = datetime.datetime.strftime(
-                    parsed_datetime, "%Y-%m-%d %H:%M"
-                )
-
-            food_entry.save()
-            food_entries = models.FoodEntry.objects.all()
-            food_entries_serializer = serializers.FoodEntrySerializer(
-                food_entries, many=True
-            )
-            return Response(
-                data=food_entries_serializer.data, status=status.HTTP_200_OK
-            )
-        elif request.method == "POST":
-
-            meal = Meal.objects.get(id=int(request.data.get("mealId")))
-
-            parsed_datetime = datetime.datetime.strptime(
-                request.data.get("foodDateTimeIntake"), "%Y-%m-%d %H:%M"
-            )
-
-            food_entry = models.FoodEntry.objects.create(
-                user=request.user,
-                meal=meal,
-                food_name=request.data.get("foodName"),
-                calories=int(request.data.get("calories")),
-                timestamp=datetime.datetime.strftime(parsed_datetime, "%Y-%m-%d %H:%M"),
-            )
-
-            food_entry_serializer = serializers.FoodEntrySerializer(
-                food_entry, many=False
-            )
-
-            return Response(status=status.HTTP_200_OK, data=food_entry_serializer.data)
-    else:
+    if not request.user.is_superuser:
         return Response(status=status.HTTP_403_FORBIDDEN)
+    if request.method == "GET":
+        food_entries = models.FoodEntry.objects.all().order_by("-timestamp")
+        food_entries_serializer = serializers.FoodEntrySerializer(
+            food_entries, many=True
+        )
+
+        return Response(
+            data=food_entries_serializer.data, status=status.HTTP_200_OK
+        )
+
+    elif request.method == "DELETE":
+
+        models.FoodEntry.objects.get(
+            id=int(request.data.get("foodEntryId"))
+        ).delete()
+
+        food_entries = models.FoodEntry.objects.all()
+        food_entries_serializer = serializers.FoodEntrySerializer(
+            food_entries, many=True
+        )
+
+        return Response(
+            data=food_entries_serializer.data, status=status.HTTP_200_OK
+        )
+
+    elif request.method == "PUT":
+        food_entry = models.FoodEntry.objects.get(
+            id=int(request.data.get("foodEntryId"))
+        )
+
+        if request.data.get("foodName"):
+            food_entry.food_name = request.data.get("foodName")
+
+        if request.data.get("calories"):
+            food_entry.calories = int(request.data.get("calories"))
+
+        if request.data.get("timestamp"):
+            parsed_datetime = datetime.datetime.strptime(
+                request.data.get("timestamp"), "%Y-%m-%d %H:%M"
+            )
+            food_entry.timestamp = datetime.datetime.strftime(
+                parsed_datetime, "%Y-%m-%d %H:%M"
+            )
+
+        food_entry.save()
+        food_entries = models.FoodEntry.objects.all()
+        food_entries_serializer = serializers.FoodEntrySerializer(
+            food_entries, many=True
+        )
+        return Response(
+            data=food_entries_serializer.data, status=status.HTTP_200_OK
+        )
+    elif request.method == "POST":
+
+        meal = Meal.objects.get(id=int(request.data.get("mealId")))
+
+        parsed_datetime = datetime.datetime.strptime(
+            request.data.get("foodDateTimeIntake"), "%Y-%m-%d %H:%M"
+        )
+
+        food_entry = models.FoodEntry.objects.create(
+            user=request.user,
+            meal=meal,
+            food_name=request.data.get("foodName"),
+            calories=int(request.data.get("calories")),
+            timestamp=datetime.datetime.strftime(parsed_datetime, "%Y-%m-%d %H:%M"),
+        )
+
+        food_entry_serializer = serializers.FoodEntrySerializer(
+            food_entry, many=False
+        )
+
+        return Response(status=status.HTTP_200_OK, data=food_entry_serializer.data)
